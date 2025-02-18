a9175020f79aa2b70e4392f3e41da34a74ac3286
Armando Quintana <aquintana@8thlight.com>
2025-02-18 10:24:36 -0600

    Moves validation to controller + adds commonDialog for error states

--- lib/features/authentication/presentation/login_screen/login_screen.dart
+++ lib/features/authentication/presentation/login_screen/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_starter_kit/features/authentication/presentation/login_screen/login_screen_controller.dart';
 import 'package:go_router/go_router.dart';
+import '../../../../common_widgets/common_dialog.dart';
 import '../../../../features/authentication/presentation/login_screen/login_widget.dart';
 import '../../../../features/routing/app_router.dart';
 
@@ -13,34 +14,33 @@ class LoginScreen extends ConsumerStatefulWidget {
 }
 
 class _LoginScreenState extends ConsumerState<LoginScreen> {
-  String _email = '';
-  String _password = '';
-
   Future<void> _onLogin() async {
-    if (_email.isNotEmpty && _password.isNotEmpty) {
-      final result = await ref
-          .read(loginScreenControllerProvider.notifier)
-          .signInWithEmailPassword(_email, _password);
+    final result = await ref
+        .read(loginScreenControllerProvider.notifier)
+        .signInWithEmailPassword();
 
-      result.when(
-        data: (_) {
-          if (mounted) {
-            context.goNamed(AppRoute.counter.name);
-          }
-        },
-        error: (err, stack) {
-          print('Login failed: $err');
-        },
-        loading: () {},
-      );
-    } else {
-      print('Error: Email and password cannot be empty.');
-    }
+    result.when(
+      data: (_) {
+        if (mounted) {
+          context.goNamed(AppRoute.counter.name);
+        }
+      },
+      error: (err, stack) {
+        showCommonDialog(
+          context: context,
+          title: 'Unable to Login',
+          content: 'Email or password is invalid',
+          primaryButtonText: 'Dismiss',
+        );
+      },
+      loading: () {},
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     final AsyncValue<void> state = ref.watch(loginScreenControllerProvider);
+    final controller = ref.read(loginScreenControllerProvider.notifier);
 
     return Center(
         child: ConstrainedBox(
@@ -51,14 +51,16 @@ class _LoginScreenState extends ConsumerState<LoginScreen> {
             child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 20),
                 child: LoginWidget(
+                  isLoginDisabled: controller.isLoginDisabled,
                   onLogin: () {
                     if (!state.isLoading) {
                       _onLogin();
                     }
                   },
-                  onEmailChanged: (value) => setState(() => _email = value),
+                  onEmailChanged: (value) =>
+                      setState(() => controller.updateEmail(value)),
                   onPasswordChanged: (value) =>
-                      setState(() => _password = value),
+                      setState(() => controller.updatePassword(value)),
                   onCreateAccount: () => context.goNamed(AppRoute.signUp.name),
                 ))));
   }
