310ba22de093757379b45e03d2fe26828b256371
Nadia Collado <ncollado@8thlight.com>
2025-03-06 15:20:29 -0500

    Add tests for additional profile fields

--- test/lib/features/profile/presentation/edit_user_profile_widget_test.dart
+++ test/lib/features/profile/presentation/edit_user_profile_widget_test.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_starter_kit/core/common_widgets/common_text_form_field.dart';
 import 'package:flutter_starter_kit/features/profile/presentation/edit_user_profile_widget.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
@@ -70,6 +69,95 @@ void main() {
       await tester.enterText(usernameField, 'New Username');
 
       verify(() => mockUsernameChanged('New Username')).called(1);
+    });
+
+    testWidgets('calls onFirstNameChanged when first name is changed',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final Finder firstNameField = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is TextField &&
+            widget.decoration?.hintText == tester.t.profile_firstName,
+      );
+
+      await tester.enterText(firstNameField, 'New First Name');
+
+      verify(() => mockFirstNameChanged('New First Name')).called(1);
+    });
+
+    testWidgets('calls onLastNameChanged when last name is changed',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final Finder lastNameField = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is TextField &&
+            widget.decoration?.hintText == tester.t.profile_lastName,
+      );
+
+      await tester.enterText(lastNameField, 'New Last Name');
+
+      verify(() => mockLastNameChanged('New Last Name')).called(1);
+    });
+
+    testWidgets('calls onPronounsChanged when pronouns are changed',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final Finder pronounsField = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is TextField &&
+            widget.decoration?.hintText == tester.t.profile_pronouns,
+      );
+
+      await tester.enterText(pronounsField, 'They/Them');
+
+      verify(() => mockPronounsChanged('They/Them')).called(1);
+    });
+
+    testWidgets('calls onAgeChanged when age is changed',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final Finder ageField = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is TextField &&
+            widget.decoration?.hintText == tester.t.profile_age,
+      );
+
+      await tester.enterText(ageField, '30');
+
+      verify(() => mockAgeChanged('30')).called(1);
+    });
+
+    testWidgets('calls onLocationChanged when location is changed',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final Finder locationField = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is TextField &&
+            widget.decoration?.hintText == tester.t.profile_location,
+      );
+
+      await tester.enterText(locationField, 'New York');
+
+      verify(() => mockLocationChanged('New York')).called(1);
+    });
+    testWidgets('calls onBioChanged when bio is changed',
+        (WidgetTester tester) async {
+      await createWidgetUnderTest(tester);
+
+      final Finder bioField = find.byWidgetPredicate(
+        (Widget widget) =>
+            widget is TextField &&
+            widget.decoration?.hintText == tester.t.profile_bio,
+      );
+
+      await tester.enterText(bioField, 'New Bio');
+
+      verify(() => mockBioChanged('New Bio')).called(1);
     });
 
     testWidgets('calls onSave when save button is pressed',
